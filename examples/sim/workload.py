5ec12cd622163efbb98c929fcbf259350ddd294b
Ole Weidner <ole.weidner@icloud.com>
2014-04-23 18:12:41 -0400

    workload description for BAC-SIM

--- examples/sim/workload.py
+++ examples/sim/workload.py
@@ -1,19 +1,21 @@
-""" (Sample) workload definition file for .
+""" (Sample) workload.
 """
 
-WORKLOAD = []
-
-# replace the paths with your input file paths on stampede
-
-for tj in range(1, 51):
-
-    task = {
-        "runtime" : 60, # minutes per task
-        "nmode"   : "/home1/00988/tg802352/MMPBSASampleDATA/nmode.5h.py",
-        "com"     : "/home1/00988/tg802352/MMPBSASampleDATA/com.top.2",
-        "rec"     : "/home1/00988/tg802352/MMPBSASampleDATA/rec.top.2",
-        "lig"     : "/home1/00988/tg802352/MMPBSASampleDATA/lig.top",
-        "traj"    : "/home1/00988/tg802352/MMPBSASampleDATA/trajectories/rep%s.traj" % tj
-    }
-
-    WORKLOAD.append(task)
+WORKLOAD = [
+{
+    "runtime"     : 30, 
+    "cores"       : 1,
+    "parmfile"    : "./complex.top",
+    "coordinates" : "./complex.pdb",
+    "conskfile"   : "./cons.pdb"
+    "input"       : "./eq0.inp"
+},
+{
+    "runtime"     : 30,
+    "cores"       : 1,
+    "parmfile"    : "./complex.top",
+    "coordinates" : "./complex.pdb",
+    "conskfile"   : "./cons.pdb"
+    "input"       : "./eq1.inp"
+}
+]
